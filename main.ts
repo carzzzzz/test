f0060581ba6de6cab35771f591edf9da1cd69195
carzzzzz <113055449+carzzzzz@users.noreply.github.com>
2022-09-07 14:10:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Yurr")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Waste Yute!")
+})
+basic.forever(function () {
+	
+})
